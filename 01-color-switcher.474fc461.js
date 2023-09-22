0e7593854b96205c3548997cb66356b74d6a826d
Imir25 <129208271+Imir25@users.noreply.github.com>
2023-09-22 20:26:55 +0000

    Deploying to gh-pages from @ Imir25/goit-js-hw-09@d46134f073e9ecd05d7d82f230de9d8a865c6b5c 🚀

--- 01-color-switcher.474fc461.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),o=document.body;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,setInterval((function(){o.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)}))}();
-//# sourceMappingURL=01-color-switcher.474fc461.js.map
